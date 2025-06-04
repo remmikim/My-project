9b441d2625e8342c1ce99dc2e7ae6d3953b45916
remmikim <joeykim383@gmail.com>
2025-06-04 17:31:58 +0900

    Add_Shape_Chg
    모양이 바뀌는 코드를 집어 넣었지만 작동하진 않습니다.

--- Assets/Scripts/MeterialChanger.cs
+++ Assets/Scripts/MeterialChanger.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
@@ -5,10 +6,15 @@ public class MaterialChanger : MonoBehaviour, IPointerEnterHandler, IPointerExit
 {
     public Material baseMaterial;
     public Material selectedMaterial;
+    public AudioClip clip;
 
     public GameObject particle;
+    public Mesh[] shapes = new Mesh[2];
     [SerializeField]
     private Renderer renderer;
+    private AudioSource source;
+    private MeshFilter filter;
+    private int index = 0;
 
     public void OnPointerEnter(PointerEventData eventData)
     {
@@ -19,20 +25,29 @@ public class MaterialChanger : MonoBehaviour, IPointerEnterHandler, IPointerExit
     {
         renderer.material = baseMaterial;
     }
-        public void OnPointerClick(PointerEventData eventData)
+    public void OnPointerClick(PointerEventData eventData)
     {
-        particle.SetActive(true);
+        particle.SetActive(!particle.activeSelf);
+        source.PlayOneShot(clip);
+        if (++index > 1)
+            index = 0;
+        filter.mesh = shapes[index];
     }
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         renderer = GetComponent<Renderer>();
         renderer.material = baseMaterial;
-    }
+        source = GetComponent<AudioSource>();
+        filter.mesh = shapes[index];
 
-    // Update is called once per frame
-    void Update()
-    {
 
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
     }
 }
